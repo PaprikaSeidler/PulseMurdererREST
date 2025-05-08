0c75f63d430c954fc7004d9dfda1accb51119023
Morten Chami Berner <mob001@edu.zealand.dk>
2025-05-08 13:08:55 +0200

    Add PlayersController and PlayerRecord for player management
    Updated PulseMurdererREST.csproj to reference PulseMurdererV3.
    Added PlayersController for handling player-related HTTP requests,
    including methods for retrieving, creating, updating, and deleting players.
    Introduced PlayerRecord to represent player data and RecordHelper
    for converting PlayerRecord to Player with necessary null checks.

---
+++ PulseMurdererREST/Controllers/PlayersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PulseMurdererV3;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PulseMurdererREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private PlayerRepository _playerRepository;
+        public PlayersController(PlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        // GET: api/<PlayersController>
+        [HttpGet]
+        public ActionResult <IEnumerable<Player>> Get()
+        {
+            IEnumerable<Player> players = _playerRepository.GetAllPlayers();
+            if (players.Count() > 0)
+            {
+                return Ok(players);
+            }
+            return NoContent();
+
+        }
+
+        // GET api/<PlayersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlayersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlayersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlayersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PulseMurdererREST/Records/PlayerRecord.cs
@@ -0,0 +1,28 @@
+﻿using PulseMurdererV3;
+
+namespace PulseMurdererREST.Records
+{
+    public record PlayerRecord(int? Id, string? Name, string? Avatar, bool IsMurderer  );
+
+    public static class RecordHelper 
+    {
+        public static Player ConvertPlayerRecord(PlayerRecord record)
+        {
+            if (record.Id == null)
+            {
+                throw new ArgumentNullException("" + record.Id);
+            }
+            if (record.Name == null)
+            {
+                throw new ArgumentNullException("" + record.Name);
+            }
+            return new Player
+            {
+                Id = (int)record.Id,
+                Name = record.Name,
+                Avatar = record.Avatar,
+                IsMurderer = record.IsMurderer
+            };
+        }
+    }
+}
